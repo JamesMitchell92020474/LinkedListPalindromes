b763e888082db3f939f99b31a0b9b7a80e18df6b
James Mitchell <jamesmitchelldesign1@gmail.com>
2019-09-03 15:26:24 +1200

    Final Commit

--- src/linkedlistpalindromes/LinkedListPalindromes.java
+++ src/linkedlistpalindromes/LinkedListPalindromes.java
@@ -5,17 +5,65 @@
  */
 package linkedlistpalindromes;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 /**
  *
- * @author 92020474
+ * LinkedListPalindromes
+ * @author James Mitchell 92020474
+ * @date 3/09/19
+ * 
  */
 public class LinkedListPalindromes {
+    
+    private static final Scanner in = new Scanner(System.in);
+    private static String input;
+    private static final LinkedList<String> palindromeList = new LinkedList();
+    private static String[] splitInput;
+    private static int palindromeLength;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        obtainingPalindrome();
+        
+        if (palindromeList.size()<=1){
+            System.out.println("Not enough letters to make a decision");
+        }
+        else{       
+            while (palindromeList.size()>=0){
+                if (palindromeList.size()>1){
+                    if (palindromeList.getFirst().equals(palindromeList.getLast())){
+                        palindromeList.removeFirst();
+                        palindromeList.removeLast();
+                    }
+                    else{
+                        System.out.println("The provided word is not a palindrome");
+                        return;
+                    }
+                }
+                else {
+                    System.out.println("The provided word is a palindrome");
+                    return;
+                }
+            }
+        }
+    }
+    
+    //obtains user input of potential palindrome
+    public static void obtainingPalindrome(){
+        System.out.println("Type in the potential palindrome\n");
+        input = in.nextLine();
+        palindromeLength = input.length();
+        splitInput = new String[palindromeLength];
+        splitInput = input.split("");
+        
+        palindromeList.addAll(Arrays.asList(splitInput));
+        
     }
     
 }
